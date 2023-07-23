7d3a8744170b592d8632441d57550253d2d021ce
P-himaja <140000346+P-himaja@users.noreply.github.com>
2023-07-23 15:24:28 +0530

    Update basics.cpp

--- basics.cpp
+++ basics.cpp
@@ -1,18 +1,19 @@
+
 #include <iostream>
-using na
+using namespace std;
     int main()
 {
-int t,rev=0;
+int t,rev=0,x;
+cout<<"enter the number";
+cin>>x;
    while (x!=0)
    {
     t=x%10;
-    if((rev>INT_MAX/10) || rev<INT_MIN/10)
-    return 0;
    rev=rev*10 +t;
     x=x/10;
-    
    }
-
-   return rev;
+if((rev>INT32_MAX/10) || rev<INT32_MIN/10)
+     cout<< "The reverse is 0";
+     else
+ cout<< "The reverse is "<<rev;
     }
-};
