2a7c7f88b3579cca92e39e01f65b34bc7dfa7f61
P-himaja <140000346+P-himaja@users.noreply.github.com>
2023-07-23 15:17:34 +0530

    Update and rename basics to basics.cpp

---
+++ basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using na
+    int main()
+{
+int t,rev=0;
+   while (x!=0)
+   {
+    t=x%10;
+    if((rev>INT_MAX/10) || rev<INT_MIN/10)
+    return 0;
+   rev=rev*10 +t;
+    x=x/10;
+    
+   }
+
+   return rev;
+    }
+};
