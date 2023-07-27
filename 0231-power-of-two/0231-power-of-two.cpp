d34112a376dd99c2cde043712487717fe8479c9a
P-himaja <140000346+P-himaja@users.noreply.github.com>
2023-07-28 01:19:54 +0530

    Time: 2 ms (47.32%), Space: 5.9 MB (79.16%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int c=0;
-     int b=n&1;
         if(n<=0)
             return false;
-    while(n!=0 )
-    {
-        int b=n&1;
-        if(b==1)
-        c++;
-        n=n>>1;
-    }
- if(c==1)
-return true;
- return false;
+        return (n&(n-1))==0;
     }
 };
