387f8fca2031ed61be8203456dc7c639e651be2c
P-himaja <140000346+P-himaja@users.noreply.github.com>
2023-07-31 23:15:13 +0530

    Create nth term func.cpp

---
+++ basics/nth term func.cpp
@@ -0,0 +1,18 @@
+    #include <iostream>
+    using namespace std;
+int ap(int n)
+{
+  return (3*n+7);
+}
+    int main()
+    { 
+      double n;
+      cout<<"Enter the value of n ";
+      cin>>n;
+         cout<<"The  nth term is "<<ap(n);
+return 0;
+ }
+      
+    
+        
+
